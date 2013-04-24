a4796b83b1ae142dd305151df17642739f4d065d
Darren Coxall <darren.coxall@simplybusiness.co.uk>
2013-04-24 09:29:35 +0100

    Added more *styling* to PDF

--- models/cv_document.rb
+++ models/cv_document.rb
@@ -16,17 +16,17 @@ module CV
 
       # Insert table header
       data << [{ content: @presenter.address, valign: :center },
-        { content: @presenter.full_name, align: :center, valign: :center },
-        { content: @presenter.phone_number, align: :right, valign: :center }]
+        { content: @presenter.full_name, align: :center, valign: :center, size: 18, font_style: :bold },
+        { content: [@presenter.phone_number, @presenter.email_address].join("\n"), align: :right, valign: :center }]
       document.table(data, column_widths: [160, 220, 160]) do
         cells.borders = []
       end
 
-      data = [[ { content: "Employment", colspan: 3 } ]]
-      @presenter.employment.each do |position|
-        data << [{ content: position.job_title, valign: :center },
-          { content: position.company_name, align: :center, valign: :center },
-          { content: position.date_range.join(" - "), align: :right, valign: :center }]
+      data = [[ { content: "EMPLOYMENT", colspan: 3, font_style: :bold, size: 14, padding: [20, 5, 0, 5] } ]]
+      @presenter.employment.first(3).each do |position|
+        data << [{ content: position.job_title, valign: :center, font_style: :bold },
+          { content: position.company_name, align: :center, valign: :center, font_style: :bold },
+          { content: position.date_range.join(" - "), align: :right, valign: :center, font_style: :bold }]
         data << [ { content: position.responsibilities, colspan: 3 } ] unless position.responsibilities.nil?
       end
       document.table(data, column_widths: [180, 180, 180]) do
@@ -37,11 +37,11 @@ module CV
         header_cells.borders = [:bottom]
       end
 
-      data = [[ { content: "Education", colspan: 3 } ]]
+      data = [[ { content: "EDUCATION", colspan: 3, font_style: :bold, size: 14, padding: [20, 5, 0, 5] } ]]
       @presenter.education.each do |school|
-        data << [{ content: school.qualification, valign: :center },
-          { content: school.establishment, align: :center, valign: :center },
-          { content: school.date_range.join(" - "), align: :right, valign: :center }]
+        data << [{ content: school.qualification, valign: :center, font_style: :bold },
+          { content: school.establishment, align: :center, valign: :center, font_style: :bold },
+          { content: school.date_range.join(" - "), align: :right, valign: :center, font_style: :bold }]
         data << [ { content: school.summary, colspan: 3 } ] unless school.summary.nil?
       end
       document.table(data, column_widths: [180, 180, 180]) do
